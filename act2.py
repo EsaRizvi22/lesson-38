e8a94515a8e73265e7e31e6f2ffdf93ed5817b60
esarizvi22 <honeyrizvi701@gmail.com>
2024-12-14 16:34:52 +0530

    Activity 2

---
+++ act2.py
@@ -0,0 +1,18 @@
+#Store two numbers of your choice in two different variables.
+#  Now perform all the Arithmetic operations on those two numbers and 
+# print the results.
+
+num1=10
+num2=20
+
+print("Sum of ",num1," and ",num2," is ",(num1+num2))
+print("Difference of ",num1," and ",num2," is ",(num1-num2))
+print("Product of ",num1," and ",num2," is ",(num1*num2))
+print("Div of ",num1," and ",num2," is ",(num2/num1))
+print("Floor Div of ",num1," and ",num2," is ",(num2//num1))
+print("Remainder of ",num1," and ",num2," is ",(num2%num1))
+print("D2 to the power of 2 ",2**2)
+
+print("is ",num1," equal t0 ",num2,num1==num2)
+print("is ",num1," greater than ",num2,num1>num2)
+print("is ",num1," smaller than ",num2,num1<num2)
